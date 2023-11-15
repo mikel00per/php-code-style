d43a15e5dcfadb1e37431209e544640fa07577eb
A. Miguel Morillo Chica <miguelmwilliams@gmail.com>
2023-11-15 19:07:17 +0100

    Update ecs.php

--- ecs.php
+++ ecs.php
@@ -7,6 +7,5 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return function (ECSConfig $ecsConfig): void {
 	$ecsConfig->paths([__DIR__ . '/src',]);
-
-    $ecsConfig->sets([CodingStyle::DEFAULT]);
+	$ecsConfig->sets([CodingStyle::DEFAULT]);
 };
