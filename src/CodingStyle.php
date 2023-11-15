2288c7f8308e0f2eb12a171f381f01e4132e75c7
A. Miguel Morillo Chica <miguelmwilliams@gmail.com>
2023-11-15 19:12:19 +0100

    Update CodingStyle.php

--- src/CodingStyle.php
+++ src/CodingStyle.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shared\Infrastructure\CodeStyle;
 
-final class CodingStyle
+final class StyleEnum
 {
 	public const DEFAULT = __DIR__ . '/coding_style.php';
 	public const ALIGNED = __DIR__ . '/coding_style_aligned.php';
